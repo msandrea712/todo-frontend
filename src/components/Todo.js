4d1b6af87615cf6e613ad95486457fc9c4726faa
Andrea Cox <msandrea712@yahoo.com>
2023-10-02 07:49:01 -0400

    added css component

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,17 +1,18 @@
 import React from 'react'
 import Check from '@mui/icons-material/Check'
 import Delete from '@mui/icons-material/Delete'
+import "../styles/todo.css"
 function Todo({
     todo
 }) {
   return (
-    <div>
+    <div className={`todo-item ${todo.completed?"completed":""}`}>
         <h3>
             {
                 todo.title
             }
         </h3>
-        <div>
+        <div className="todo-actions">
           <Check/>
           <Delete/>
         </div>
