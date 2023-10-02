ad12cb4e1005e8cc3a792b3abd4c7201535495ef
Andrea Cox <msandrea712@yahoo.com>
2023-10-02 07:56:16 -0400

    added form for new todo

--- src/pages/NewTodo.js
+++ src/pages/NewTodo.js
@@ -5,7 +5,26 @@ function NewTodo() {
   return (
     <RootLayout>
       <main>
-        newtodo
+        <h2>
+          create new todo
+        </h2>
+        <form>
+          <div>
+            <label>
+              todo title
+              <input/>
+            </label>
+          </div>
+          <div>
+            <label>
+              completed
+              <input/>
+            </label>
+          </div>
+          <button type="submit">
+            create todo
+          </button>
+        </form>
       </main>
     </RootLayout>
   )
