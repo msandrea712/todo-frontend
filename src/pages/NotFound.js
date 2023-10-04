928cab74b9272f0ab1a47282ffbec4c395cee8b4
Andrea Cox <msandrea712@yahoo.com>
2023-10-04 09:45:27 -0400

    set up notfound home page with css

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,9 +1,34 @@
 import React from 'react'
-
+import {Link} from "react-router-dom"
+import "../styles/notfound.css"
 function NotFound() {
   return (
-    <div>NotFound</div>
-  )
+    <div className="error-page">
+      {" "}
+      <p className="error-message">
+        {" "}
+        Oops! The page you're looking for doesn't exist.{" "}
+      </p>{" "}
+      <p className="error-description">
+        {" "}
+        It seems you've reached an unknown route. Please check the URL or
+        navigate back to the home page using the navigation links.{" "}
+      </p>{" "}
+      <p className="error-contact">
+        {" "}
+        If you believe this is an issue with our website, please contact our
+        support team at{" "}
+        <a href="mailto:support@example.com" className="error-link">
+          {" "}
+          support@example.com{" "}
+        </a>{" "}
+        <Link to="/" className="error-link">
+          {" "}
+          Back to Home{" "}
+        </Link>{" "}
+      </p>{" "}
+    </div>
+  );
 }
 
 export default NotFound
